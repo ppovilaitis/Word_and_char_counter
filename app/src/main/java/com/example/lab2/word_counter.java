acdbc68160842423457a0bb1523e32a33c57b80a
ppovilaitis <povilaitispranas@gmail.com>
2022-12-16 19:59:38 +0200

    added tests codes and updated code to fix errors

--- app/src/main/java/com/example/lab2/word_counter.java
+++ app/src/main/java/com/example/lab2/word_counter.java
@@ -4,13 +4,8 @@ import android.widget.Toast;
 
 public class word_counter {
     public static int getWordsCount(String text) {
-        int wordCount = 1;
-        System.out.println(text.length());
-        for (int i = 0; i<text.length(); i++) {
-            if (String.valueOf(text.charAt(i)).equals(" ") || String.valueOf(text.charAt(i)).equals(".") || String.valueOf(text.charAt(i)).equals(",")) {
-                wordCount++;
-            }
-        }
-        return wordCount;
+        int temp=0;
+        String[] words =text.trim().split("\\s+");
+        return (words.length);
     }
 }
--- app/src/test/java/com/example/lab2/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab2/ExampleUnitTest.java
@@ -10,8 +10,46 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void wordCount_onlyCharNoSpace() {
+        final String inputString = "prisikiskiakopusteliaudavomemesvisadakiekvienadienairbuvomelaimingi";
+        final String expectedOutcome = "1";
+
+        assertEquals(expectedOutcome, Integer.toString(word_counter.getWordsCount(inputString)));
     }
+
+    @Test
+    public void wordCount_onlyCharWithSpaces() {
+        final String inputString = "prisikiskiakopusteliaudavome mes visada kiekviena diena ir buvome laimingi";
+        final String expectedOutcome = "8";
+
+        assertEquals(expectedOutcome, Integer.toString(word_counter.getWordsCount(inputString)));
+    }
+
+    @Test
+    public void wordCount_onlyCharMultipleSpace() {
+        final String inputString = "prisikiskiakopusteliaudavome  mes visada   kiekviena diena ir  buvome laimingi";
+        final String expectedOutcome = "8";
+
+        assertEquals(expectedOutcome, Integer.toString(word_counter.getWordsCount(inputString)));
+    }
+
+    @Test
+    public void charCount_onlyWordsNoSpace() {
+        final String inputString = "prisikiskiakopusteliaudavomemesvisadakiekvienadienairbuvomelaimingi";
+        final String expectedOutcome = "67";
+
+        assertEquals(expectedOutcome, Integer.toString(TextCounter.getCharsCount(inputString)));
+    }
+
+    @Test
+    public void charCount_onlyWordsWithSpaces() {
+        final String inputString = "prisikiskiakopusteliaudavome  mes visada   kiekviena diena ir  buvome laimingi";
+        final String expectedOutcome = "78";
+
+        assertEquals(expectedOutcome, Integer.toString(TextCounter.getCharsCount(inputString)));
+    }
+
 }
+
